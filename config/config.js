e798c286f4ea55c9990a48b14306f49ebe14e388
tsejx <tsejx@foxmail.com>
2020-03-20 23:56:32 +0800

    config: use dumi to build

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export default {
+  mode: 'site',
+  title: 'DevOps Guidebook',
+  description: 'DevOps 完全知识体系',
+  publicPath: '/devops-guidebook/',
+  base: '/devops-guidebook/',
+  logo: '/favicon.png',
+  favicon: './favicon.ico',
+  navs: [
+    null,
+    {
+      title: 'Github',
+      path: 'https://github.com/tsejx/devops-guidebook',
+    },
+  ],
+};
